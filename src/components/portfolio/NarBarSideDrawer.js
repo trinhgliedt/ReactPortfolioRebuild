f574676e34bd27a8c6d8bcba8870d27fc0736697
Trinh Gliedt <chuot2008@gmail.com>
2021-02-04 00:09:34 -0800

    removed material UI

--- src/components/portfolio/NarBarSideDrawer.js
+++ src/components/portfolio/NarBarSideDrawer.js
@@ -1,88 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-import {
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-} from "@material-ui/core";
-
-import { Menu } from "@material-ui/icons";
-import { makeStyles } from "@material-ui/core/styles";
-import { sizing, palette } from "@material-ui/system";
-
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  linkText: {
-    textDecoration: `none`,
-    textTransform: `uppercase`,
-    color: `black`,
-  },
-});
 
 const NarBarSideDrawer = ({ navLinks }) => {
-  const [state, setState] = useState({ right: false });
-  const classes = useStyles();
-
-  const Box = styled.div`
-    ${sizing}${palette}
-  `;
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ [anchor]: open });
-  };
-  const sideDrawerList = (anchor) => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List component="nav">
-        {navLinks.map(({ title, path, icon }) => (
-          <Link to={path} key={title} className={classes.linkText}>
-            <ListItem button>
-              <ListItemIcon>{icon}</ListItemIcon>
-              <ListItemText primary={title} />
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    </div>
-  );
-  return (
-    <div>
-      <IconButton
-        edge="start"
-        aria-label="menu"
-        onClick={toggleDrawer("right", true)}
-      >
-        <Menu />
-      </IconButton>
-      <Box width="100%" bgcolor="secondary.main">
-        Test
-      </Box>
-      <Drawer
-        anchor="right"
-        open={state.right}
-        onClose={toggleDrawer("right", false)}
-      >
-        {sideDrawerList("right")}
-      </Drawer>
-    </div>
-  );
+  return <div>Nav bar side drawer</div>;
 };
 
 export default NarBarSideDrawer;
--- src/components/portfolio/NavBar.js
+++ src/components/portfolio/NavBar.js
@@ -1,73 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  makeStyles,
-  Container,
-} from "@material-ui/core";
-import Hidden from "@material-ui/core/Hidden";
-import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
-
-import NavLinks from "./NavLinks";
-import NavbarSideDrawer from "./NarBarSideDrawer";
-
-const useStyles = makeStyles({
-  navbarDisplayFlex: {
-    display: `flex`,
-    justifyContent: `space-between`,
-  },
-  navDisplayFlex: {
-    display: `flex`,
-    justifyContent: `space-between`,
-  },
-  linkText: {
-    textDecoration: `none`,
-    textTransform: `uppercase`,
-    color: `white`,
-  },
-});
-
 const NavBar = () => {
-  const classes = useStyles();
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Container maxWidth="lg" className={classes.navbarDisplayFlex}>
-            <IconButton edge="start" color="inherit" aria-label="home">
-              <Link to="/">
-                <HomeOutlinedIcon fontSize="large" />
-              </Link>
-            </IconButton>
-            <Hidden smDown>
-              <List
-                component="nav"
-                aria-labelledby="main navigation"
-                className={classes.navDisplayFlex}
-              >
-                {NavLinks.map(({ title, path }) => (
-                  <Link to={path} key={title} className={classes.linkText}>
-                    <ListItem button>
-                      <ListItemText primary={title} />
-                    </ListItem>
-                  </Link>
-                ))}
-              </List>
-            </Hidden>
-            <Hidden mdUp>
-              <NavbarSideDrawer navLinks={NavLinks} />
-            </Hidden>
-          </Container>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+  return <div>Nav bar</div>;
 };
 
 export default NavBar;
