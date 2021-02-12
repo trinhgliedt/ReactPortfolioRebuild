5eede93e69f481cb0398fcc0158a06c1066e3788
Trinh Gliedt <chuot2008@gmail.com>
2021-02-12 00:03:28 -0800

    resume

--- src/pages/portfolio/Resume/Resume.js
+++ src/pages/portfolio/Resume/Resume.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./styles.scss";
 import resumePic from "../../../assets/portfolio/resumePic.jpg";
+import { phoneIcon } from "../../../components/portfolio/Resume/ResumeIcons";
 
 const Resume = () => {
   return (
     <div className="container">
       <h1 className="main-title text-center">Resume</h1>
-      <div className="resumeWrap d-flex justify-content-center">
+      <div className="resumeWrap">
         <div className="resume">
           <div className="resumeLeft">
-            <img src={resumePic} width="60%" alt="resume" />
+            <div className="d-flex justify-content-center mb-3">
+              <img src={resumePic} width="60%" alt="resume" />
+            </div>
+            <p className="sectionTitle">CONTACT</p>
+            <div className="">
+              {phoneIcon}
+              <span className="col-10">+1 (510) 396-6601</span>
+            </div>
           </div>
           <div className="resumeRight">
             <h5 className="">Trinh Gliedt</h5>
