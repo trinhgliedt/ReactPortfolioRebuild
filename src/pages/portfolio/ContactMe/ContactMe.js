d1a03cefbb4e4a25cad03e6f6e30ba2a4908ce8e
Trinh Gliedt <chuot2008@gmail.com>
2021-02-17 00:03:04 -0800

    Contact me form

--- src/pages/portfolio/ContactMe/ContactMe.js
+++ src/pages/portfolio/ContactMe/ContactMe.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./styles.scss";
 
 const ContactMe = () => {
   return (
-    <div className="container">
-      <h1>Contact Me</h1>
+    <div className="container contactPage">
+      <h1 className="text-center main-title py-3">Contact Me</h1>
+      <div className="row">
+        <form id="contactForm" lassName="col-6 ">
+          <div className="d-flex justify-content-between ">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+          </div>
+          <input type="text" placeholder="Subject" />
+          <textarea rows="8" placeholder="Message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 };
