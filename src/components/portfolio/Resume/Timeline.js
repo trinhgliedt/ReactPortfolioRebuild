4440f7b524bfced8fdeb89debba5b6b46bea5fa6
Trinh Gliedt <chuot2008@gmail.com>
2021-02-16 16:36:07 -0800

    basic resume done

--- src/components/portfolio/Resume/Timeline.js
+++ src/components/portfolio/Resume/Timeline.js
@@ -5,20 +5,27 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 const timeline = {
-  class: "vertical-timeline-element--work",
+  class: "vertical-timeline-element--work my-4",
   contentStyle: {
-    background: "rgba(25, 97, 117, 0.637)",
+    background: "rgba(45, 45, 46, 0.157)",
+    border: "1px solid black",
     color: "#fff",
     padding: "0.2rem 0.5rem",
     boxShadow: "none",
   },
-  contentArrowStyle: { borderRight: "7px solid rgba(25, 97, 117, 0.637)" },
+  contentArrowStyle: { borderRight: "7px solid darkGrey" },
+  dateClassName: "smallFont",
   iconStyle: {
     background: "rgb(70, 83, 87)",
-    color: "red",
-    width: "1.8rem",
-    height: "1.8rem",
-    transform: "translate(1rem,1rem)",
+    margin: "1rem 0.6rem",
+    width: "1.2rem",
+    height: "1.2rem",
+  },
+  iconStyleSpecial: {
+    background: "rgb(5, 108, 117)",
+    margin: "1rem 0.6rem",
+    width: "1.2rem",
+    height: "1.2rem",
   },
   titleClass: "vertical-timeline-element-title m-0 smallFont text-warning",
   companyClass: "vertical-timeline-element-subtitle m-0 smallFont",
@@ -26,14 +33,14 @@ const timeline = {
 
 const VerticalTimeLine = () => {
   return (
-    <VerticalTimeline>
+    <VerticalTimeline layout="1-column" className=" py-2">
       <VerticalTimelineElement
         className={timeline.class}
         contentStyle={timeline.contentStyle}
         contentArrowStyle={timeline.contentArrowStyle}
         date="Nov 2020 - Jan 2021"
-        iconStyle={timeline.iconStyle}
-        // icon={<WorkIcon />}
+        dateClassName={timeline.dateClassName}
+        iconStyle={timeline.iconStyleSpecial}
       >
         <p className={timeline.titleClass}>Full Stack Developer Volunteer</p>
         <p className={timeline.companyClass}>Coding Dojo</p>
@@ -43,8 +50,8 @@ const VerticalTimeLine = () => {
         contentStyle={timeline.contentStyle}
         contentArrowStyle={timeline.contentArrowStyle}
         date="Sep 2015 - May 2020"
+        dateClassName={timeline.dateClassName}
         iconStyle={timeline.iconStyle}
-        // icon={<WorkIcon />}
       >
         <p className={timeline.titleClass}>Accounting Manager</p>
         <p className={timeline.titleClass}>Senior Accountant</p>
@@ -55,8 +62,8 @@ const VerticalTimeLine = () => {
         contentStyle={timeline.contentStyle}
         contentArrowStyle={timeline.contentArrowStyle}
         date="Jun 2013 – Sep 2015"
+        dateClassName={timeline.dateClassName}
         iconStyle={timeline.iconStyle}
-        // icon={<WorkIcon />}
       >
         <p className={timeline.titleClass}>Senior Accountant</p>
         <p className={timeline.companyClass}>Allen & Cook, Inc.</p>
@@ -66,8 +73,8 @@ const VerticalTimeLine = () => {
         contentStyle={timeline.contentStyle}
         contentArrowStyle={timeline.contentArrowStyle}
         date="Jun 2011 – Mar 2013"
+        dateClassName={timeline.dateClassName}
         iconStyle={timeline.iconStyle}
-        // icon={<WorkIcon />}
       >
         <p className={timeline.titleClass}>Accounting Supervisor</p>
         <p className={timeline.titleClass}>Staff Accountant</p>
